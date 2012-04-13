e2cc5deb38eba6fe1b47309eac0dd2de5ff4bae7
Andrew Rhee <adrhee@gmail.com>
2012-04-12 17:11:39 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shieldformen::Application.config.secret_token = '4e518bfe12fb6e4b9f36ecb75f015e903bce710967efd7b50e727cf3c62bc8f8f382c655ccbeef32bb44c2ff13bdea1cb7c029e62141d76e403496a8a01a7660'
